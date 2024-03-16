67ea517b8ee25b7bef190eb171200da25a90d073
h4zan <h4zan@hotmail.com>
2024-03-16 22:00:28 +0100

    💄(style) fix footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,10 @@ export const Footer = ({ menuToggled }: FooterProps) => {
   }
   return (
     <>
-      <footer className="socials absolute bottom-28 2xl:bottom-36 -rotate-90 -left-[4.8rem] text-base md:text-lg mb-2 md:pb-4 font-medium">
-        <ul className="social-links flex justify-center items-start space-x-1 list-square marker:text-accentColor saturate-50 gap-3">
-          <li className="social-link">
+      <footer className="socials absolute bottom-32 md:bttom-36 2xl:bottom-36 -rotate-90 -left-[4.8rem] text-base md:text-lg mb-2 md:pb-4 font-medium">
+        <ul className="social-links flex justify-center items-start space-x-1 gap-3">
+          <li className="social-link flex flex-row items-center justify-center">
+            <span className="social-link-bullet w-1.5 h-1.5 md:w-2 md:h-2 inline-block bg-accentColor saturate-50 "></span>
             <a
               href="https://github.com/h4zan"
               data-tooltip-target="tooltip-github"
@@ -17,10 +18,13 @@ export const Footer = ({ menuToggled }: FooterProps) => {
               target="_blank"
               aria-label="Github link"
             >
-              <span className="">GitHub</span>
+              {" "}
+              <span className=""> GitHub</span>
             </a>
           </li>
-          <li className="social-link">
+          <li className="social-link flex flex-row items-center justify-center">
+            <span className="social-link-bullet w-1.5 h-1.5 md:w-2 md:h-2 inline-block bg-accentColor saturate-50 "></span>
+
             <a
               href="https://www.linkedin.com/in/hazany/"
               data-tooltip-target="tooltip-linkedin"
@@ -28,7 +32,8 @@ export const Footer = ({ menuToggled }: FooterProps) => {
               target="_blank"
               aria-label="LinkedIn link)"
             >
-              <span className=""> LinkedIn</span>
+              {" "}
+              <span className="">LinkedIn</span>
             </a>
           </li>
         </ul>
